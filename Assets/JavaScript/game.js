8dab3d4b018bf6432a4ab1dee0db1f602d17e6a7
Polaris <tristynkcline@gmail.com>
2019-09-24 08:49:31 -0500

    HTML CSS JS Update

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -2,14 +2,26 @@ var crystalArray = [1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12];
 
 var crystalCounter = crystalArray[Math.floor(Math.random() * crystalArray.length)];
 
-$("#magicNumber").text(crystalCounter);
+var magicNumArray = [19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58, 61, 64, 67, 70, 73, 76, 79, 82, 85, 88, 91, 94, 97, 100, 103, 106, 109, 112, 115, 118, 120]
+
+var magicNumCount = magicNumArray[Math.floor(Math.random() * magicNumArray.length)];
+
+$("#crystal1").append(crystalCounter);
+
+$("#magicNumber").text(magicNumCount);
 
 $("#crystal1").on("click", function() {
 
-crystalCounter++;
-console.log(crystalCounter);
+if (playerScore === crystalCounter)
+    
+    playerScore++;
+    console.log(crystalCounter);
 
 
 
 });
+
+
+
+
 
